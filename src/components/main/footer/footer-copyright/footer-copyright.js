27f6bada9e822f2256e385ca1a113cf25268ac85
Joey Schroeder <schrjos@gmail.com>
2019-07-29 21:02:39 -0500

    Add dynamic data to <FooterCopyright />

--- src/components/main/footer/footer-copyright/footer-copyright.js
+++ src/components/main/footer/footer-copyright/footer-copyright.js
@@ -4,18 +4,25 @@ import { Link } from 'react-router-dom';
 import { ROUTES } from 'constants/routes';
 import React from 'react';
 import logo from 'images/db-tax-logo-dark.png';
+import moment from 'moment';
 import styles from './footer-copyright.style.scss';
 
-export const FooterCopyright = () => (
-  <div className={styles.root}>
-    <div className={styles.logoContainer}>
-      <Link to={ROUTES.HOME}>
-        <img src={logo} alt={COMPANY_NAME} className={styles.logo} />
-      </Link>
+export const FooterCopyright = () => {
+  const year = moment().format('YYYY');
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.logoContainer}>
+        <Link to={ROUTES.HOME}>
+          <img src={logo} alt={COMPANY_NAME} className={styles.logo} />
+        </Link>
+      </div>
+      <div className={styles.copyright}>
+        ©{year} {COMPANY_NAME}. All Rights Reserved.
+      </div>
+      <div className={styles.social}>
+        <IconButton iconName="facebook-f" iconType="brand" />
+      </div>
     </div>
-    <div className={styles.copyright}>© 2019 DB Tax & Financial Services, Inc. All Rights Reserved.</div>
-    <div className={styles.social}>
-      <IconButton iconName="facebook-f" iconType="brand" />
-    </div>
-  </div>
-);
+  );
+};
