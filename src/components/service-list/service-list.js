64331a57e4c5ff770de9997f447c5137450cc0b1
Joey Schroeder <schrjos@gmail.com>
2019-08-06 12:16:06 -0500

    Add <ServiceList />

---
+++ src/components/service-list/service-list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { CHILDREN_PROP_TYPE } from 'constants/children-prop-type';
+import styles from './service-list.style.scss';
+
+export const ServiceList = props => {
+  const { children } = props;
+  return <div className={styles.root}>{children}</div>;
+};
+
+ServiceList.propTypes = {
+  children: CHILDREN_PROP_TYPE
+};
+
+ServiceList.defaultProps = {
+  children: null
+};
