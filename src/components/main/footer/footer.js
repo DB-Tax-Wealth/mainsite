37ec3a779eb2d3aab439533de65771de83f9fadd
Joey Schroeder <schrjos@gmail.com>
2019-08-06 12:15:19 -0500

    Add CSS utils

--- src/components/main/footer/footer.js
+++ src/components/main/footer/footer.js
@@ -1,13 +1,12 @@
 import { FooterCallToAction } from 'components/main/footer/footer-call-to-action/footer-call-to-action';
 import { FooterSiteMap } from 'components/main/footer/footer-site-map/footer-site-map';
 import React from 'react';
-import styles from 'components/main/footer/footer.style.scss';
 import { FooterCopyright } from './footer-copyright/footer-copyright';
 
 export const Footer = () => {
   return (
-    <div className={styles.root}>
-      <div className={styles.content}>
+    <div className="color-background--quaternary color--white padding--all">
+      <div className="max-width--xl margin--horizontal--auto">
         <FooterCallToAction />
         <FooterSiteMap />
         <FooterCopyright />
