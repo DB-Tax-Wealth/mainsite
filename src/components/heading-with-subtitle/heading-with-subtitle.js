0570a992726e6d7a88043be6757d230c63e7bac8
Joey Schroeder <schrjos@gmail.com>
2019-08-14 22:17:18 -0500

    Add subtitleColor prop to <HeadingWithSubtitle />

--- src/components/heading-with-subtitle/heading-with-subtitle.js
+++ src/components/heading-with-subtitle/heading-with-subtitle.js
@@ -1,15 +1,17 @@
 import { CHILDREN_PROP_TYPE } from 'constants/children-prop-type';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import React from 'react';
 import styles from './heading-with-subtitle.style.scss';
 
 export const HeadingWithSubtitle = props => {
-  const { children, subtitle } = props;
+  const { children, subtitle, subtitleColor } = props;
   const subtitleExists = Boolean(subtitle);
+  const subtitleClassName = classnames(styles.subtitle, { [`color--${subtitleColor}`]: subtitleColor });
 
   return (
     <div className={styles.root}>
-      {subtitleExists && <div className={styles.subtitle}>{subtitle}</div>}
+      {subtitleExists && <div className={subtitleClassName}>{subtitle}</div>}
       <h1>{children}</h1>
     </div>
   );
@@ -17,10 +19,12 @@ export const HeadingWithSubtitle = props => {
 
 HeadingWithSubtitle.propTypes = {
   children: CHILDREN_PROP_TYPE,
-  subtitle: PropTypes.string
+  subtitle: PropTypes.string,
+  subtitleColor: PropTypes.string
 };
 
 HeadingWithSubtitle.defaultProps = {
   children: null,
-  subtitle: ''
+  subtitle: '',
+  subtitleColor: 'secondary'
 };
