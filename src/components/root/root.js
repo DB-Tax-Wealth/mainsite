94036355f391b174ba22043dfe8688e0fc44cd10
Joey Schroeder <schrjos@gmail.com>
2019-09-15 17:06:16 -0500

    Revert "Add <NotFound /> screen"
    This reverts commit 2008da52c7d87e1dc9dc6cf2c296b5b1aa0d1efa.

--- src/components/root/root.js
+++ src/components/root/root.js
@@ -1,11 +1,10 @@
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import React from 'react';
 import { ROUTES_CONFIG } from 'constants/routes';
 import { Provider } from 'react-redux';
 import { App } from 'components/app/app';
 import PropTypes from 'prop-types';
-import { NotFound } from 'components/screens/not-found/not-found';
 import { RouteWithSubRoutes } from './route-with-sub-routes/route-with-sub-routes';
 
 export const Root = props => {
@@ -15,24 +14,21 @@ export const Root = props => {
     <Provider store={store}>
       <HashRouter hashType="noslash">
         <App>
-          <Switch>
-            {Object.keys(ROUTES_CONFIG).map(key => {
-              const route = ROUTES_CONFIG[key];
-              const { component, exact, path, routes } = route;
+          {Object.keys(ROUTES_CONFIG).map(key => {
+            const route = ROUTES_CONFIG[key];
+            const { component, exact, path, routes } = route;
 
-              return (
-                <RouteWithSubRoutes
-                  component={component}
-                  exact={exact}
-                  key={key}
-                  path={path}
-                  routeKey={key}
-                  routes={routes}
-                />
-              );
-            })}
-            <Route component={NotFound} />
-          </Switch>
+            return (
+              <RouteWithSubRoutes
+                component={component}
+                exact={exact}
+                key={key}
+                path={path}
+                routeKey={key}
+                routes={routes}
+              />
+            );
+          })}
         </App>
       </HashRouter>
     </Provider>
