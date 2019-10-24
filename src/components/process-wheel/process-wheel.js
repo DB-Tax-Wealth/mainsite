cb8571e704eb22f0b75d71b1730e59d75c0a38ee
Joey Schroeder <schrjos@gmail.com>
2019-10-23 23:15:56 -0500

    Add <ProcessWheel />

---
+++ src/components/process-wheel/process-wheel.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import colors from 'styles/config/variables.scss';
+import Color from 'color';
+import styles from './process-wheel.style.scss';
+import { ProcessWheelSection } from './process-wheel-section/process-wheel-section';
+
+export const ProcessWheel = props => {
+  const { activeIndex, color, sections, strokeWidth, title } = props;
+
+  const sectionsLength = sections.length;
+
+  const radius = 50;
+  const radiusAdjusted = radius - strokeWidth / 2;
+  const circumferenceAdjusted = Math.PI * (radiusAdjusted * 2);
+  const radiusShift = circumferenceAdjusted / sectionsLength;
+
+  const strokeDasharray = [radiusShift, circumferenceAdjusted];
+  const sectionDegrees = 360 / sectionsLength;
+  const rotationOffset = sectionDegrees / 2;
+  const rotation = -activeIndex * sectionDegrees - (rotationOffset - 90);
+
+  return (
+    <div className={styles.root}>
+      <svg viewBox="0 0 100 100">
+        <circle cx={50} cy={50} fill="none" r={radiusAdjusted} strokeWidth={strokeWidth} stroke="transparent" />
+      </svg>
+      <h2 className={styles.title}>{title}</h2>
+      <svg viewBox="0 0 100 100" className={styles.section} style={{ transform: `rotate(${rotation}deg)` }}>
+        {sections.map(({ onClick, message, label }, index) => {
+          const key = `section-${index}`;
+
+          const strokeColor = Color(color)
+            .darken(index * 0.1)
+            .hex();
+
+          const labelDx = -25;
+          const messageDx = -25;
+
+          return (
+            <ProcessWheelSection
+              active={index === activeIndex}
+              className={styles.wheelSection}
+              color={strokeColor}
+              key={key}
+              label={label}
+              labelDx={labelDx}
+              message={message}
+              messageDx={messageDx}
+              onClick={onClick}
+              radius={radiusAdjusted}
+              rotation={(360 / sectionsLength) * index}
+              strokeDasharray={strokeDasharray}
+              strokeWidth={strokeWidth}
+            />
+          );
+        })}
+      </svg>
+    </div>
+  );
+};
+
+ProcessWheel.propTypes = {
+  activeIndex: PropTypes.number,
+  color: PropTypes.string,
+  sections: PropTypes.arrayOf(PropTypes.object),
+  strokeWidth: PropTypes.number,
+  title: PropTypes.string
+};
+
+ProcessWheel.defaultProps = {
+  activeIndex: 0,
+  color: colors.secondary,
+  sections: [],
+  strokeWidth: 20,
+  title: 'Our Process'
+};
--- src/components/screens/wealth/wealth-process/wealth-process.js
+++ src/components/screens/wealth/wealth-process/wealth-process.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Screen } from 'components/layout/screen/screen';
 import { Hero } from 'components/layout/hero/hero';
 import { Section } from 'components/layout/section/section';
@@ -6,126 +6,87 @@ import { Section } from 'components/layout/section/section';
 import { Grid } from 'components/layout/grid/grid';
 import { Row } from 'components/layout/grid/row/row';
 import { Column } from 'components/layout/grid/column/column';
-import styles from './wealth-process.style.scss';
+import { ProcessWheel } from 'components/process-wheel/process-wheel';
+import { Icon } from 'components/layout/icon/icon';
 
-export const WealthProcess = () => {
-  const strokeWidth = 20;
-  const sections = 6;
+export class WealthProcess extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeProcessIndex: 0 };
+  }
 
-  const diameter = 100;
-  const radius = diameter / 2;
-  const radiusAdjusted = radius - strokeWidth / 2;
-  const circumferenceAdjusted = Math.PI * (radiusAdjusted * 2);
-  const radiusShift = circumferenceAdjusted / sections;
+  handleOnSectionClick(activeProcessIndex) {
+    this.setState({ activeProcessIndex });
+  }
 
-  return (
-    <Screen>
-      <Hero title="Process" subtitle="Wealth">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus magna, venenatis ut ligula eu, lobortis
-          consectetur orci.
-        </p>
-      </Hero>
-      <Section color="primary-light">
-        <Grid className="padding--horizontal--none">
-          <Row className="margin--bottom">
-            <Column>
-              <h2>Lorem ipsum dolor sit amet</h2>
-              <p>
-                Ut pretium egestas dolor, quis suscipit arcu luctus nec. Donec pulvinar, orci faucibus pellentesque
-                sodales, arcu augue pulvinar nulla, sit amet sodales urna enim eu tortor. Donec convallis odio et
-                vestibulum hendrerit. Ut viverra euismod posuere.
-              </p>
-            </Column>
-          </Row>
-          <Row>
-            <Column widthMd={5} className="margin--xs--bottom margin--md--bottom--none">
-              <div className={styles.root}>
-                <svg viewBox="0 0 100 100" className={styles.circle}>
-                  {[...Array(sections)].map((x, index) => {
-                    const strokeDasharray = [radiusShift, circumferenceAdjusted];
-                    const strokeDashoffset = -radiusShift * index;
-                    const pathId = `path-${index}`;
+  render() {
+    const { activeProcessIndex } = this.state;
 
-                    return (
-                      <>
-                        {/* <svg viewBox="0 0 500 500"> */}
-                        <path
-                          id={pathId}
-                          fill="transparent"
-                          d="
-                                M 50, 50
-                                a 25,25 0 1,1 50,0
-                                a 25,25 0 1,1 -50,0
-                              "
-                        />
-                        <text>
-                          <textPath xlinkHref={`#${pathId}`}>Process 1</textPath>
-                        </text>
-                        {/* </svg> */}
-                        <circle
-                          cx="50%"
-                          className={styles.section}
-                          key={pathId}
-                          cy="50%"
-                          fill="none"
-                          r={radiusAdjusted}
-                          strokeDasharray={strokeDasharray}
-                          strokeDashoffset={strokeDashoffset}
-                          strokeWidth={strokeWidth}
-                        />
-                      </>
-                    );
-                  })}
-                </svg>
-                {/* <svg viewBox="0 0 100 100">
-                    {[...Array(sections)].map((x, index) => {
-                      const strokeDasharray = [radiusShift, circumferenceAdjusted];
-                      const strokeDashoffset = -radiusShift * index;
+    const processes = [
+      {
+        label: 'Review',
+        message: 'Monitor the Plan',
+        icon: 'file-contract',
+        onClick: () => this.handleOnSectionClick(0)
+      },
+      {
+        label: 'Engage',
+        message: 'Define the Relationship',
+        icon: 'users',
+        onClick: () => this.handleOnSectionClick(1)
+      },
+      {
+        label: 'Collect',
+        message: 'Understand Client Goals',
+        icon: 'search',
+        onClick: () => this.handleOnSectionClick(2)
+      },
+      { label: 'Analyze', message: 'Analyze & Evaluate', icon: 'signal', onClick: () => this.handleOnSectionClick(3) },
+      { label: 'Design', message: 'Create & Revise', icon: 'lightbulb', onClick: () => this.handleOnSectionClick(4) }
+      // {
+      //   label: 'Implement',
+      //   message: 'Implement Client Decisions',
+      //   icon: 'cogs',
+      //   onClick: () => this.handleOnSectionClick(5)
+      // }
+    ];
 
-                      return (
-                        <>
-                          <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
-                          <text width="500">
-                            <textPath alignmentBaseline="top" xlinkHref="#curve">
-                              Dangerous Curves Ahead
-                            </textPath>
-                          </text>
-                        </>
-                      );
-                    })}
-                  </svg> */}
-              </div>
-            </Column>
-            <Column offsetMd={1} widthMd={6} className="display--flex">
-              <div className="border-radius--all color-background--white padding--all shadow--primary">
-                <h3>Lorem ipsum dolor sit amet</h3>
-                <p>
-                  Ut pretium egestas dolor, quis suscipit arcu luctus nec. Donec pulvinar, orci faucibus pellentesque
-                  sodales, arcu augue pulvinar nulla, sit amet sodales urna enim eu tortor.
-                </p>
-                <p>Donec convallis odio et vestibulum hendrerit. Ut viverra euismod posuere.</p>
-                <p>
-                  Integer imperdiet placerat luctus. Vivamus dignissim finibus est vel fermentum. Sed ut lobortis sem.
-                  Etiam a iaculis urna, sed rutrum ligula. Mauris volutpat, lorem sit amet aliquam lobortis, nibh elit
-                  interdum erat, sed consectetur sapien nibh a nibh
-                </p>
-              </div>
-            </Column>
-          </Row>
-        </Grid>
-        {/* <div className={styles.root}>
-            <svg viewBox="0 0 500 500">
-              <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
-              <text>
-                <textPath xlinkHref="#curve">Process 1</textPath>
-                <textPath xlinkHref="#curve">Process 1</textPath>
-                <textPath xlinkHref="#curve">Process 1</textPath>
-                <textPath xlinkHref="#curve">Process 1</textPath>
-              </text>
-            </svg>
-          </div> */}
-      </Section>
-    </Screen>
-  );
-};
+    return (
+      <Screen>
+        <Hero title="Process" subtitle="Wealth">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus magna, venenatis ut ligula eu, lobortis
+            consectetur orci.
+          </p>
+        </Hero>
+        <Section color="primary-light">
+          <Grid className="padding--horizontal--none">
+            <Row>
+              <Column widthMd={5} className="margin--xs--bottom margin--md--bottom--none">
+                <ProcessWheel sections={processes} activeIndex={activeProcessIndex} />
+              </Column>
+              <Column offsetMd={1} widthMd={6} className="display--flex">
+                <div className="border-radius--all color-background--white padding--all shadow--primary">
+                  <h3 className="margin--bottom--none">
+                    <Icon name={processes[activeProcessIndex].icon} /> {processes[activeProcessIndex].label}
+                  </h3>
+                  <h5 className="margin--top--half">{processes[activeProcessIndex].message}</h5>
+                  <p>
+                    Ut pretium egestas dolor, quis suscipit arcu luctus nec. Donec pulvinar, orci faucibus pellentesque
+                    sodales, arcu augue pulvinar nulla, sit amet sodales urna enim eu tortor.
+                  </p>
+                  <p>Donec convallis odio et vestibulum hendrerit. Ut viverra euismod posuere.</p>
+                  <p>
+                    Integer imperdiet placerat luctus. Vivamus dignissim finibus est vel fermentum. Sed ut lobortis sem.
+                    Etiam a iaculis urna, sed rutrum ligula. Mauris volutpat, lorem sit amet aliquam lobortis, nibh elit
+                    interdum erat, sed consectetur sapien nibh a nibh
+                  </p>
+                </div>
+              </Column>
+            </Row>
+          </Grid>
+        </Section>
+      </Screen>
+    );
+  }
+}
