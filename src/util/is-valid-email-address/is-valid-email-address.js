d3701d8caa957d1d8230317dff6a5cc04083cf9b
Joey Schroeder <schrjos@gmail.com>
2019-11-12 14:07:11 -0600

    feat: Add isValidEmailAddress util

---
+++ src/util/is-valid-email-address/is-valid-email-address.js
@@ -0,0 +1,6 @@
+const regExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export const isValidEmailAddress = email => {
+  if (!email) return false;
+  return regExp.test(String(email).toLowerCase());
+};
