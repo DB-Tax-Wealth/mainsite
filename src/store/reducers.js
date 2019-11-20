ae883309723b0d9b2751312b96fe1366ccbf8255
Joey Schroeder <schrjos@gmail.com>
2019-11-20 15:16:55 -0600

    feat: Add alerts reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { accountAccessReducer } from './account-access/account-access';
 import { contactFormReducer } from './contact-form/contact-form';
+import { alertsReducer } from './alerts/alerts';
 
 export const reducers = combineReducers({
   accountAccess: accountAccessReducer,
+  alerts: alertsReducer,
   contactForm: contactFormReducer
 });
