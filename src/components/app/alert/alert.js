ac9b23931834c1047b80c8e8185de30250a977f3
Joey Schroeder <schrjos@gmail.com>
2019-11-20 15:17:32 -0600

    feat: Add AlertContainer and Alert

---
+++ src/components/app/alert/alert.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { CHILDREN_PROP_TYPE } from 'constants/children-prop-type';
+import styles from './alert.style.scss';
+
+export const Alert = props => {
+  const { children, color, title } = props;
+
+  const className = classNames(styles.root, { [styles[color]]: color });
+  const titleExists = Boolean(title);
+
+  return (
+    <div className={className}>
+      {titleExists && <div className={styles.heading}>{titleExists && <h3 className={styles.title}>{title}</h3>}</div>}
+      {children}
+    </div>
+  );
+};
+
+Alert.propTypes = {
+  children: CHILDREN_PROP_TYPE,
+  color: PropTypes.oneOf(['danger', 'success']),
+  title: PropTypes.string
+};
+
+Alert.defaultProps = {
+  children: null,
+  color: '',
+  title: ''
+};
