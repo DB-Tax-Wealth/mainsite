ac772ce6fda817d57993d86d80495c88d4d98c4f
Joey Schroeder <schrjos@gmail.com>
2019-11-25 09:52:10 -0600

    chore: Add team photo to Team page

--- src/components/screens/team/team.js
+++ src/components/screens/team/team.js
@@ -6,6 +6,8 @@ import { Row } from 'components/layout/grid/row/row';
 import { Column } from 'components/layout/grid/column/column';
 import { TEAM_MEMBERS } from 'constants/team-members';
 import { Hero } from 'components/layout/hero/hero';
+import { Image } from 'components/layout/image/image';
+import TeamPhoto from 'images/db-tax-family-team.jpg';
 import { TeamMember } from './team-member/team-member';
 
 const teamMembers = TEAM_MEMBERS.map(({ shortBio, id, img, name, title }) => (
@@ -23,6 +25,26 @@ export const Team = () => {
           to our clients.
         </p>
       </Hero>
+      <Section color="primary">
+        <Grid>
+          <Row>
+            <Column widthMd={6} className="margin--xs--bottom margin--md--bottom--none">
+              <h2>The DB Tax Family</h2>
+              <p>
+                We believe our team oriented approach adds a broader perspective to all we do and provides increased
+                benefits to our clients.
+              </p>
+              <p>
+                Our experienced professionals have helped many people just like you with similar issues and concerns. We
+                specialize in creating an approach that is designed to address your individual situation.
+              </p>
+            </Column>
+            <Column widthMd={6}>
+              <Image src={TeamPhoto} />
+            </Column>
+          </Row>
+        </Grid>
+      </Section>
       <Section color="primary-light">
         <Grid className="padding--horizontal--none">
           <Row>{teamMembers}</Row>
