fdc82ae3cc8d0839e34793cacf808eed51a9d8ca
Joey Schroeder <schrjos@gmail.com>
2020-05-06 12:32:38 -0500

    fix: Process links

--- src/components/screens/accounting/accounting-process/accounting-process-organize/accounting-process-organize.js
+++ src/components/screens/accounting/accounting-process/accounting-process-organize/accounting-process-organize.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ROUTES } from 'constants/routes';
 
 export const AccountingProcessOrganize = () => (
   <div>
     <p>
       Your business is unique and requires a personalized approach to planning. In order to deliver the best experience,
-      we will <a href="#services/accounting/documents">provide check-lists and tools</a> to gather the necessary
+      we will <Link to={ROUTES.ACCOUNTING_QUICK_LINKS.path}>provide check-lists and tools</Link> to gather the necessary
       information for optimal results tailored to your needs. Not only will this process help satisfy your tax reporting
       requirements, it will also allow us to analyze and deliver recommendations on how to save taxes and protect your
       business.
--- src/components/screens/wealth/wealth-process/wealth-process-team/wealth-process-team.js
+++ src/components/screens/wealth/wealth-process/wealth-process-team/wealth-process-team.js
@@ -1,12 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ROUTES } from 'constants/routes';
 
 export const WealthProcessTeam = () => (
   <div>
     <p>
       Your financial story is unique. In order to build a snapshot of your financial picture help our team understand
-      your situation. You will <a href="#services/wealth/documents">provide the following documents</a> prior to your
-      next meeting. These items are necessary to design your financial plan.
+      your situation. You will <Link to={ROUTES.WEALTH_DOCUMENTS.path}>provide the following documents</Link> prior to
+      your next meeting. These items are necessary to design your financial plan.
     </p>
     <p>See relevant documents:</p>
     <ul>
