3e9646b0936e674a87b30efa06e68634bdc1e81e
Joey Schroeder <schrjos@gmail.com>
2020-05-06 09:59:53 -0500

    feat: Remove SmartVault link
    This is only commented out, because eventually it will come back, but
    currently it should be removed from the site.

--- src/components/app/account-access-modal/account-access-modal.js
+++ src/components/app/account-access-modal/account-access-modal.js
@@ -5,14 +5,14 @@ import { Row } from 'components/layout/grid/row/row';
 import { Column } from 'components/layout/grid/column/column';
 import { Button } from 'components/layout/button/button';
 import { AVANTAX_LINK } from 'constants/avantax-link';
-import { SMARTVAULT_LINK } from 'constants/smartvault-link';
+// import { SMARTVAULT_LINK } from 'constants/smartvault-link';
 import PropTypes from 'prop-types';
 import { Icon } from 'components/layout/icon/icon';
 
 const openLinkInNewTab = (href) => Object.assign(document.createElement('a'), { target: '_blank', href }).click();
 
 const onAvantaxClick = () => openLinkInNewTab(AVANTAX_LINK);
-const onSmartvaultClick = () => openLinkInNewTab(SMARTVAULT_LINK);
+// const onSmartvaultClick = () => openLinkInNewTab(SMARTVAULT_LINK);
 
 export const AccountAccessModal = (props) => {
   const { active, onOverlayClick } = props;
@@ -24,16 +24,16 @@ export const AccountAccessModal = (props) => {
       </h4>
       <Grid className="padding--horizontal--none">
         <Row>
-          <Column className="margin--xs--bottom--half margin--sm--bottom">
+          <Column>
             <Button onClick={onAvantaxClick} style={{ width: '100%' }} color="secondary">
               Avantax Access
             </Button>
           </Column>
-          <Column>
+          {/* <Column>
             <Button onClick={onSmartvaultClick} style={{ width: '100%' }} color="secondary">
               SmartVault
             </Button>
-          </Column>
+          </Column> */}
         </Row>
       </Grid>
     </ModalOverlay>
